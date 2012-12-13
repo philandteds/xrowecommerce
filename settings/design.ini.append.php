962fa679cde17162387a1d9e5eb92764a00705f1
Geoff Bentley <geoff@philandteds.com>
2012-12-14 12:56:11 +1300

    commented out conflicting javascript files

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -4,9 +4,9 @@
 DesignExtensions[]=xrowecommerce
 
 [JavaScriptSettings]
-FrontendJavaScriptList[]=ezjsc::jquery
-FrontendJavaScriptList[]=ezjsc::jqueryio
-FrontendJavaScriptList[]=xrowecommerce.js
+#FrontendJavaScriptList[]=ezjsc::jquery
+#FrontendJavaScriptList[]=ezjsc::jqueryio
+#FrontendJavaScriptList[]=xrowecommerce.js
 BackendJavaScriptList[]=xrowproductvariation.js
 #JavaScriptList[]=xrowproductvariation.js
 #JavaScriptList[]=xrowecommerce.js
@@ -14,6 +14,6 @@ BackendJavaScriptList[]=xrowproductvariation.js
 [StylesheetSettings]
 BackendCSSFileList[]=xrowproductvariation.css
 BackendCSSFileList[]=backend.css
-FrontendCSSFileList[]=frontend.css
+#FrontendCSSFileList[]=frontend.css
 
 */ ?>
