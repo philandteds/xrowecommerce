821fe36952faa4da1ef86a009e51f3c984bd813f
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2013-06-24 06:46:44 +1200

    using  eZPreferences for orerlist limit

--- modules/xrowecommerce/orderlist.php
+++ modules/xrowecommerce/orderlist.php
@@ -31,8 +31,15 @@ $Module = $Params['Module'];
 
 $tpl = eZTemplate::factory();
 
+if( (int) $Params['Limit'] > 0 ) {
+	eZPreferences::setValue( 'admin_orderlist_limit', (int) $Params['Limit'] );
+}
+
 $offset = $Params['Offset'];
-$limit  = (int) $Params['Limit'] > 0 ? (int) $Params['Limit'] : 15;
+$limit  = (int) eZPreferences::value( 'admin_orderlist_limit' );
+if( $limit <= 0 ) {
+	$limit = 15;
+}
 
 if ( eZPreferences::value( 'admin_orderlist_sortfield' ) )
 {
