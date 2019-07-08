660997c5f4606687d11e35754d39a884656019bc
Kirk Mayo <kirk@philandteds.com>
2019-07-08 15:53:16 +1200

    Check permission on the payment gateway

--- modules/xrowecommerce/set_payment_gateway.php
+++ modules/xrowecommerce/set_payment_gateway.php
@@ -13,15 +13,37 @@ if( !is_object( $order ) ) {
 $ini = eZINI::instance();
 $order->detachProductCollection();
 if( $ini->variable( 'ShopSettings', 'ClearBasketOnCheckout' ) == 'enabled' ) {
-	$basket = eZBasket::currentBasket();
-	$basket->remove();
+    $basket = eZBasket::currentBasket();
+    $basket->remove();
 }
 
 if(
-	$Params['Gateway'] !== null
-	&& in_array( $Params['Gateway'], xrowEPayment::allowedGatewayListByUser() )
+    $Params['Gateway'] !== null
+    && in_array( $Params['Gateway'], xrowEPayment::allowedGatewayListByUser() )
 ) {
-	$http->setSessionVariable( 'paymentgateway', $Params['Gateway'] );
+    $http->setSessionVariable( 'paymentgateway', $Params['Gateway'] );
+}
+
+// some controller checking to confirm they can use the relevant gateway
+$pass = true;
+switch ( $Params['Gateway'] ) {
+    case 'xrowCashOnDeliveryGateway' :
+        // Only country is UAE currently
+        $pass = eZINI::instance( 'shopping.ini' )->variable('General', 'FreeGatewayEnabled' )
+            == 'enabled';
+        break;
+    case 'xrowAdvancepayment' :
+        //TODO review business logic required for advanced payment
+        // but disabling for now
+        $pass = false;
+        break;
+    case 'free' :
+        $pass = eZINI::instance( 'shopping.ini' )->variable('General', 'FreeGatewayEnabled' )
+            == 'enabled';
+        break;
+}
+if ($pass === true) {
+    return $Params['Module']->handleError( eZError::KERNEL_NOT_AVAILABLE, 'kernel' );
 }
 
 return $Params['Module']->redirectTo( '/xrowecommerce/checkout/' );
